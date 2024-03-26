bfc8e3c248b85da9a710d42ce79eeba5f2c97f2c
Isamu Nakamura <60463247+IsamuNakamura@users.noreply.github.com>
2024-03-27 08:52:28 +0900

    add locals module (#7)

---
+++ infra/terraform/modules/locals/main.tf
@@ -0,0 +1,11 @@
+# 共通のローカル変数は、プロジェクトによって変更する
+locals {
+  domain_name             = "terraform_test.jp"
+  frontend_subdomain_name = "app"
+  backend_subdomain_name  = "api"
+
+  health_check_path             = "health_check"
+  frontend_port                 = 3000
+  backend_port                  = 80
+  discovery_service_domain_name = "local"
+}
---
+++ infra/terraform/modules/locals/outputs.tf
@@ -0,0 +1,27 @@
+output "domain_name" {
+  value = local.domain_name
+}
+
+output "frontend_subdomain_name" {
+  value = local.frontend_subdomain_name
+}
+
+output "backend_subdomain_name" {
+  value = local.backend_subdomain_name
+}
+
+output "health_check_path" {
+  value = local.health_check_path
+}
+
+output "frontend_port" {
+  value = local.frontend_port
+}
+
+output "backend_port" {
+  value = local.backend_port
+}
+
+output "discovery_service_domain_name" {
+  value = local.discovery_service_domain_name
+}
